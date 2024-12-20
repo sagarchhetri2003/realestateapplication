b0900492d706b728362cef056f94f15009ecb70b
sagarchhetri2003 <sagarkshetri0000@gmail.com>
2024-12-20 11:30:05 +0545

    Added bottomnavigators in dashboard successfully

--- lib/view/dashboard_screen_view.dart
+++ lib/view/dashboard_screen_view.dart
@@ -1,20 +1,74 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class DashboardScreenView extends StatelessWidget {
+// class DashboardScreenView extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Admin Dashboard'),
+//       ),
+//       body: Center(
+//         child: Text(
+//           'This is Admin Dashboard',
+//           style: TextStyle(
+//             fontSize: 24,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+import 'package:flutter/material.dart';
+import 'package:realestateapplication/view/bottom_navigator_screen/Search_screen_view.dart';
+import 'package:realestateapplication/view/bottom_navigator_screen/favorite_screen_view.dart';
+import 'package:realestateapplication/view/bottom_navigator_screen/home_screen_view.dart';
+import 'package:realestateapplication/view/bottom_navigator_screen/profile_screen_view.dart';
+
+class DashboardScreenView extends StatefulWidget {
+  const DashboardScreenView({super.key});
+
+  @override
+  State<DashboardScreenView> createState() => _DashboardScreenViewState();
+}
+
+class _DashboardScreenViewState extends State<DashboardScreenView> {
+  int _selectedIndex = 0;
+  List<Widget> lstBottomScreen = [
+    const HomeScreenView(),
+    const SearchScreenView(),
+    const FavoriteScreenView(),
+    const ProfileScreenView(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: Text('Admin Dashboard'),
+        title: const Text('Dashboard'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0,
       ),
-      body: Center(
-        child: Text(
-          'This is Admin Dashboard',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favorite'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
