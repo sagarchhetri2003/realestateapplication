317e72a3aa70e4758b056f5eabb209a0417acd9f
sagarchhetri2003 <sagarkshetri0000@gmail.com>
2024-12-20 11:27:30 +0545

    Added Profile screen view navigator in dash

---
+++ lib/view/bottom_navigator_screen/profile_screen_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreenView extends StatelessWidget {
+  const ProfileScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile'),
+    );
+  }
+}
